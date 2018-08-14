a5df6847f87db1fd770b6b20048ae9cd7d9d09d1
chauyan.wang <wang.chauyan@gmail.com>
2018-08-15 00:49:18 +0800

    feat: Implement CircleImageView for circle image
    Implement CircleImageView class by using BitmapShader
    Add a test image file for placeholder

--- app/src/main/java/idv/chauyan/shopbackquiz/views/CircleImageView.kt
+++ app/src/main/java/idv/chauyan/shopbackquiz/views/CircleImageView.kt
@@ -1,13 +1,22 @@
 package idv.chauyan.shopbackquiz.views
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Paint
+import android.graphics.*
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
+
 
 class CircleImageView : android.support.v7.widget.AppCompatImageView {
 
-    val paint:Paint = Paint()
+
+    var drawRes: Drawable? = null
+    var bitmap: Bitmap? = null
+
+    var paint = Paint().apply {
+        isAntiAlias = true
+    }
+
 
 
     constructor(context: Context) : super(context) {}
@@ -17,7 +26,54 @@ class CircleImageView : android.support.v7.widget.AppCompatImageView {
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {}
 
     override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
+        //super.onDraw(canvas)
+        if (drawable == null) return
 
+        loadBitmap()
+
+        if (bitmap == null) return
+
+        val circleCenter = Math.min(canvas!!.width, canvas.height) / 2.0f
+        canvas.drawCircle(circleCenter, circleCenter, circleCenter, paint)
+    }
+
+    fun loadBitmap() {
+        if (drawRes == drawable)
+            return
+        drawRes = drawable
+        bitmap = drawableToBitmap(drawRes)
+        updateShader()
+    }
+
+    fun drawableToBitmap(drawable: Drawable?): Bitmap? {
+        if (drawable == null) return null
+        if (drawable is BitmapDrawable) drawable.bitmap
+
+        try {
+            val bitmap = Bitmap.createBitmap(drawable.intrinsicWidth, drawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+            val canvas = Canvas(bitmap)
+            drawable.setBounds(0, 0, canvas.width, canvas.height)
+            drawable.draw(canvas)
+
+            return bitmap
+        }
+        catch (e :Exception) {
+            e.printStackTrace()
+        }
+
+        return null
+    }
+
+    fun updateShader() {
+        if (bitmap == null) return
+
+        val shader = BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
+        var scale = Math.min(width, height) * 1.0f / Math.min(bitmap!!.width, bitmap!!.height) * 1.0f
+
+        val matrix = Matrix()
+        matrix.setScale(scale, scale)
+        shader.setLocalMatrix(matrix)
+
+        paint.setShader(shader)
     }
 }
