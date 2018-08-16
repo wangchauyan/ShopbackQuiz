d1a2e2ff900f71c8fd2948bacaacc73ef6e124fe
chauyan.wang <wang.chauyan@gmail.com>
2018-08-17 01:33:55 +0800

    feat: Add Github Apis test case
    Add two test cases for getting user list & getting user detail information

--- app/src/test/java/idv/chauyan/shopbackquiz/GithubApiUnitTest.kt
+++ app/src/test/java/idv/chauyan/shopbackquiz/GithubApiUnitTest.kt
@@ -1,12 +1,51 @@
 package idv.chauyan.shopbackquiz
 
-import org.junit.Assert.assertEquals
+import idv.chauyan.shopbackquiz.repository.NetworkRepository
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import org.junit.Test
-
+import kotlin.test.assertNotNull
 
 class GithubApiUnitTest {
+
+    val comDisposable = CompositeDisposable()
+    val respository = NetworkRepository.getInstance()
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun getUsers_Success() {
+        comDisposable.add(
+                respository
+                        .getUsers(0, 100)
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeOn(Schedulers.io())
+                        .subscribe({
+                            result ->
+                            assert(result.isNotEmpty())
+                        }, {
+                            error ->
+                            error.printStackTrace()
+                        })
+        )
+    }
+
+    @Test
+    fun getUserDetailInfo_Success() {
+        val userName = "mojombo"
+
+        val respository = NetworkRepository.getInstance()
+        comDisposable.add(
+                respository
+                        .getUserDetail(userName)
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeOn(Schedulers.io())
+                        .subscribe({
+                            result ->
+                            assertNotNull(result)
+                        }, {
+                            error ->
+                            error.printStackTrace()
+                        })
+        )
     }
 }
